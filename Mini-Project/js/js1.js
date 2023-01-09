d4142695c8ae5fd6edafca7b324d73f2e9e07a33
Ashwin Anand <92919909+Ashuvwxyz@users.noreply.github.com>
2023-01-09 23:22:10 +0530

    Added final changes

--- Mini-Project/js/js1.js
+++ Mini-Project/js/js1.js
@@ -101,10 +101,70 @@ function redirect() {
     var a4 = (document.getElementById("pass").value).length;
     var a5 = (document.getElementById("re_pass").value).length;
     if (a1 === 0 || a2 === 0 || a3 === 0 || a4 === 0 || a5 === 0 ){
-        alert("Please fill in all the required details.");
+        alert("Please fill in all the required details marked with '*'.");
     }
     else{
         alert("Successfully registered! You will be redirected to the login page.");
         location.href = "login.html";
+    }
+}
+
+/*
+Profile JS
+Dyanesh S
+CB.EN.U4CYS21015
+*/
+
+function validateFname() {
+    var mailformat = (/^[a-zA-Z]+$/);
+    var x = document.getElementById("i5").value;
+    if (!x.match(mailformat)) {
+        alert("You have entered an invalid first name!");
+        return false;
+    }
+    return true;
+}
+
+function validateLname() {
+    var mailformat = (/^[a-zA-Z]+$/);
+    var x = document.getElementById("i6").value;
+    if (!x.match(mailformat)) {
+        alert("You have entered an invalid last name!");
+        return false;
+    }
+    return true;
+}
+
+function validateContactEmail() {
+    var mailformat = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    var x = document.getElementById("i1").value;
+    if (!x.match(mailformat)) {
+        alert("You have entered an invalid email address!");
+        return false;
+    }
+    return true;
+}
+
+function submitToAPI(e) {
+            
+    // e.preventDefault();
+
+    var firstname = document.getElementById("i5").value;
+    var lastname = document.getElementById("i6").value;
+    var emailaddress = document.getElementById("i1").value;
+    var message = document.getElementById("description-input").value;
+    if (firstname=="" || lastname=="" || emailaddress=="" || message=="")
+        {
+            alert("Please Fill All Required Field");
+            return false;
+        }
+
+    else
+    {
+        if(validateFname() && validateLname() && validateContactEmail())
+    {
+        alert("We will contact soon!");
+
+    }
     }
 }
